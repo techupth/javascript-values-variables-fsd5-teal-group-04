82f3467717cd0e8f1dd80ac1370bd7d93e71447e
Palm <palml1lax@gmail.com>
2023-11-23 12:04:23 +0700

    feat: finish exercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,13 @@
 //Exercise #1: Variable Creation
 //Start coding here
 
+let userName = "Karter";
+let userSurname = "Anderson";
+let userLocation = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
 //Exercise #2: Variable Reassignment
 //Start coding here
+
+const restaurantName = "Chef's Table by Chef James";
+const totalPrice = 15000;
+const avaliableRestaurant = 1500;
